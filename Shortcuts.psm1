37981ee6d8a813757315ecb6e411d69cb4eb9424
Dave Amenta <dave@daveamenta.com>
2014-01-10 12:47:05 -0800

    init

---
+++ Shortcuts.psm1
@@ -0,0 +1,14 @@
+﻿
+function New-Shortcut($TargetPath, $ShortcutPath) {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+    $Shortcut.TargetPath = $TargetPath
+    $Shortcut.Save()
+}
+
+function New-FavoriteShortcut($TargetPath, $DisplayName) {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Links\$DisplayName.lnk")
+    $Shortcut.TargetPath = $TargetPath
+    $Shortcut.Save()
+}
